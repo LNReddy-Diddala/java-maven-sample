f70815f9fa93d546d8b296659e62235aceab0fde
LNReddy-Diddala <36847836+LNReddy-Diddala@users.noreply.github.com>
2018-02-27 08:59:11 +0000

    Build automation testing...

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Welcome Mr.LNReddy Diddala!" );
+        System.out.println( "Build automation done." );
     }
 }
